7062f39b6a4bab0d2277863335d45d1e798bfe26
Chareef-kulem <chareef2555@gmail.com>
2020-09-10 11:43:43 +0700

    Nx

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CharacterCard from './CharacterCard';
+import WorldCard from './WorldCard';
+import './App.css'
+function App(){
+  return(
+    <div>
+  
+        <WorldCard value="hello" className="card"/>
+    
+    </div>
+  )
+}
+export default App;
---
+++ src/WorldCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CharacterCard from './CharacterCard';
+const activationHandler = c => { console.log(`${c} has been activated.`) }
+export default function WorldCard(props){
+    return (
+        <div>
+            { Array.from(props.value).map((c, i) => 
+            <CharacterCard value={c} key={i} activationHandler={activationHandler}/>)}
+            
+        </div>
+    );
+}
+
